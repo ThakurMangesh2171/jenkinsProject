7e1abeb331545be289e57c8adbe58a9ddf859fdf
ThakurMangesh2171 <mangesh.thakur@vassarlabs.com>
2023-11-02 11:54:15 +0530

    Backend :: git webhook test 2

--- FirstService/src/main/java/com/springGateWay/GateWay/controller/MessageController.java
+++ FirstService/src/main/java/com/springGateWay/GateWay/controller/MessageController.java
@@ -20,7 +20,7 @@ public class MessageController {
 
 //        LOGGER.info("IN First Server :: getMessage"+id);
         System.out.println("In First Server :: "+id);
-        System.out.println("hello Jenkins with webhook");
+        System.out.println("hello Jenkins with webhook test");
         return "This is message of First-microservice";
 
     }
